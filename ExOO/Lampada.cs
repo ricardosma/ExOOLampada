23d9dcc3946fc204493c94f70c9ddc9442a0f4ac
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2022-10-25 21:19:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExOO/Lampada.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExOO
+{
+    internal class Lampada
+    {
+        public bool ligada;
+        public double potencia;
+        public string marca;
+        public string cor;
+        public void desligaLampada()
+        {
+            if(!ligada)//if (ligada == false)
+            {
+                Console.WriteLine("Já está desligada!");
+               
+            }
+            else//if(ligada == true)
+            {
+                ligada = false;
+                Console.WriteLine("Desligando lâmpada!");
+            }
+        }
+
+        public void ligaLampada()
+        {
+            if(ligada)//if(ligada == true)
+            {
+                Console.WriteLine("Já está ligada!");
+            }
+            else
+            {
+                ligada = true;
+                Console.WriteLine("Ligando Lâmpada!");
+            }
+        }
+        public void mudaCor(string novacor)
+        {
+            if(cor == novacor)
+            {
+                Console.WriteLine("A cor "+novacor+" já era a cor configurada!");
+            }
+            else
+            {
+                cor = novacor;
+                Console.WriteLine("A nova cor da lâmpada é: "+cor);
+            }
+
+        }
+
+        public void apresentaDados()
+        {
+            Console.WriteLine("A lampada de marca " + marca);
+            Console.WriteLine("Tem " + potencia + " wats");
+            Console.Write("A cor da lampada está: ");
+            if (cor == "azul")
+            {                
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (cor == "verde")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(cor);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            if (ligada)
+            {
+                Console.WriteLine("A mimosa tá ligada!");
+            }
+            else
+            {
+                Console.WriteLine("Ta desmaiada!");
+            }
+        }
+
+        
+        //public void 
+    }
+}
---
+++ ExOO/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Hello, World!");
+Console.Write("teste");
+Console.ReadLine();*/
+
+using ExOO;
+Lampada l = new Lampada();
+Console.WriteLine("Digite a marca da lampada: ");
+l.marca = Console.ReadLine();
+Console.WriteLine("Digite a potência da lampada: ");
+l.potencia = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a cor da lampada: ");
+l.cor = Console.ReadLine();
+Console.WriteLine("Digite 1 para ligar a lampada ou 0 para desligar: ");
+int op = int.Parse(Console.ReadLine());
+if (op == 1)
+{
+    l.ligada = true;
+}
+else
+{
+    l.ligada = false;
+}
+
+while (true)
+{
+    Console.WriteLine("Digite 1 para mudar a cor da lâmpada: ");
+    Console.WriteLine("Digite 2 para desligar a lâmpada: ");
+    Console.WriteLine("Digite 3 para ligar a lâmpada: ");
+    Console.WriteLine("Digite 4 para apresentar os dados da lâmpada: ");
+    Console.WriteLine("Digite 5 para sair do programa: ");
+
+    op = int.Parse(Console.ReadLine());
+    if(op == 1)
+    {
+        Console.WriteLine("Digite a nova cor da lampada: ");
+        string nvcor = Console.ReadLine();
+        l.mudaCor(nvcor);
+        Console.Clear();
+    }
+    else if(op == 2)
+    {
+        Console.Clear();
+        l.desligaLampada();
+
+    }
+    else if(op == 3)
+    {
+        Console.Clear();
+        l.ligaLampada();
+    }
+    else if(op == 4)
+    {
+        Console.Clear();
+        l.apresentaDados();
+
+    }
+    else if (op == 5)
+    {
+        break;
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Toma tenecia e digita um valor correto!");
+    }
+
+}
+
+
+
+
+
+l.ligaLampada();
+l.apresentaDados();
+
+
+
+
+
+
